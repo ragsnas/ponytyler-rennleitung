78b7c144bbcfd3a1970c0317631aded754edd88b
dustin <ragsnas@googlemail.com>
2024-06-12 19:34:26 +0200

    improves duplicate tool ui by showing origin of each song

--- ui/rl/projects/song/src/lib/song-input/create-song.component.ts
+++ ui/rl/projects/song/src/lib/song-input/create-song.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { SongService } from '../../public-api';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Origin, SongService} from '../../public-api';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'lib-song-input',
@@ -9,18 +9,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-song.component.scss']
 })
 export class CreateSongComponent {
-  
+
   public form: FormGroup = new FormGroup({
     name: new FormControl(''),
     artist: new FormControl(''),
     deleted: new FormControl(false),
     selectable: new FormControl(true),
   });
-  
+
   constructor(private songService: SongService, private router: Router) { }
 
   createSong() {
-    this.songService.createSong(this.form.getRawValue()).subscribe({
+    this.songService.createSong({
+      ...this.form.getRawValue(),
+      deleted: false,
+      origin: Origin.FROM_DIRECT_INPUT
+    }).subscribe({
       next: (result) => {
         console.log(`It worked:`, result);
         this.router.navigate(['song']);
