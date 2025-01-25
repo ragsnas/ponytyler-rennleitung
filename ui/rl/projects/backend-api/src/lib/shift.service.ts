d88e880e1b5f5f5ef9357aed9a44195e4ea82b4c
Dustin Tyler <rennleitung@ponytyler.de>
2025-01-25 20:41:23 +0100

    stash

--- ui/rl/projects/backend-api/src/lib/shift.service.ts
+++ ui/rl/projects/backend-api/src/lib/shift.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from "@angular/core";
 import { Show } from "./show.service";
 
 export interface Shift {
-  id: number;
-  show: Show;
-  showId: number;
+  id: number | undefined;
+  show: Show | undefined;
+  showId: number | undefined;
   order: number;
-  duration: number;
-  shiftStarted: Date;
-  shiftFinished: Date;
+  duration: number | undefined;
+  shiftStarted: Date | undefined;
+  shiftFinished: Date | undefined;
   shiftJobs: ShiftRole[];
 }
 
 export interface ShiftRole {
-  id: number;
-  shift?: Shift;
-  shiftId?: number;
+  id: number | undefined;
+  shift?: Shift | undefined;
+  shiftId?: number | undefined;
   roleTitle: string;
   crewMember: string;
 }
