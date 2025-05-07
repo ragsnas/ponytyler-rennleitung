4b2a969b019bc7bd1347fc43e438b9a7c6d6e4ad
Dustin Tyler <rennleitung@ponytyler.de>
2025-05-07 15:23:53 +0200

    fixed/improved update script

--- update_pi.sh
+++ update_pi.sh
@@ -1,11 +1,24 @@
+echo "----- STARTING PONY TYLER RENNLEITUNG UPDATE --------"
 cd ~/ponytyler-rennleitung
+echo "-- BACKUP DATABASE ----------------------------------"
 sudo cp ~/ponytyler-rennleitung/be/prisma/*.db ~/rl_db_backup_$EPOCHSECONDS.db
+echo "-- PULL FROM REPOSITORY -----------------------------"
 git pull
+echo "-- COPY UI TO NGINX HTML FOLDER ---------------------"
 sudo cp ~/ponytyler-rennleitung/ui/rl/dist/rl/*.* /var/www/html/
 sudo chmod a+r /var/www/html/*.*
+echo "-- OVERWRITE NGINX CONFIG ---------------------------"
 sudo cp ~/ponytyler-rennleitung/ui/rl/nginx_config /etc/nginx/sites-enabled/default
+echo "-- RESTART NGINX ------------------------------------"
 sudo systemctl start nginx
 sudo systemctl restart nginx
+echo "-- BUILD BACKEND ------------------------------------"
 cd ~/ponytyler-rennleitung/be
 npm i
+npx prisma generate
+npx prisma generate --sql
+npx prisma migrate deploy
+npm run build
+echo "-- RESTART BACKEND SERVICE --------------------------"
 pm2 restart all
+echo "----- PONY TYLER RENNLEITUNG UPDATE FINISHED --------"
