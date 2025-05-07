25f2f65c34ac7e61c517d9d41cfa2f11292ba4b9
Dustin Tyler <rennleitung@ponytyler.de>
2025-05-07 22:06:26 +0200

    adjust update script

--- update_pi.sh
+++ update_pi.sh
@@ -1,18 +1,18 @@
 printf "\n\n----- STARTING PONY TYLER RENNLEITUNG UPDATE ------\n"
 cd ~/ponytyler-rennleitung
 printf "\n- BACKUP DATABASE ---------------------------------\n"
-sudo cp ~/ponytyler-rennleitung/be/prisma/*.db ~/rl.db
-sudo cp ~/ponytyler-rennleitung/be/prisma/*.db ~/rl_db_backup_$EPOCHSECONDS.db
+sudo cp --verbose ~/ponytyler-rennleitung/be/prisma/*.db ~/rl.db
+sudo cp --verbose ~/ponytyler-rennleitung/be/prisma/*.db ~/rl_db_backup_$EPOCHSECONDS.db
 printf "\n- PULL FROM REPOSITORY ----------------------------\n"
 git reset --hard
 git pull
 printf "\n- COPY UI TO NGINX HTML FOLDER --------------------\n"
-sudo cp ~/ponytyler-rennleitung/ui/rl/dist/rl/browser/*.* /var/www/html/
-sudo chmod a+r /var/www/html/*.*
+sudo cp --verbose ~/ponytyler-rennleitung/ui/rl/dist/rl/browser/*.* /var/www/html/
+sudo chmod -v a+r /var/www/html/*.*
 printf "\n- OVERWRITE NGINX CONFIG --------------------------\n"
-sudo cp ~/ponytyler-rennleitung/ui/rl/nginx_config /etc/nginx/sites-enabled/default
+sudo cp --verbose ~/ponytyler-rennleitung/ui/rl/nginx_config /etc/nginx/sites-enabled/default
 printf "\n- RESTORE DATABASE BACKUP -------------------------\n"
-sudo cp ~/rl.db ~/ponytyler-rennleitung/be/prisma/rl.db
+sudo cp --verbose ~/rl.db ~/ponytyler-rennleitung/be/prisma/rl.db
 printf "\n- RESTART NGINX -----------------------------------\n"
 sudo systemctl start nginx
 sudo systemctl restart nginx
